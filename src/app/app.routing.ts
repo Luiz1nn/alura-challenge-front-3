76ea422855a22ae216457b892c588bf4e117cbe0
Luis Fernando <luisfernandobr2003@gmail.com>
2022-03-30 10:10:34 -0300

    tsconfig update and update AppRouting

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  {
+    path: '', component: AuthLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import("@app/layouts/auth-layout/auth-layout.module").then(m => m.AuthLayoutModule)
+      }
+    ]
+  },
+  {
+    path: '', component: AdminLayoutComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import("@app/layouts/admin-layout/admin-layout.module").then(m => m.AdminLayoutModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
